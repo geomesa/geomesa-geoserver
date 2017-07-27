31fe9343a88f393955abe43b6f3b2aec273c84d9
Austin Heyne <aheyne@ccri.com>
2017-07-27 16:32:33 -0400

    More small speedups and fixed bug
    Signed-off-by: Austin Heyne <aheyne@ccri.com>

--- geomesa-gs-styling/src/main/scala/org/geomesa/gs/styling/filter/GeoMesaLabelParser.scala
+++ geomesa-gs-styling/src/main/scala/org/geomesa/gs/styling/filter/GeoMesaLabelParser.scala
@@ -22,13 +22,16 @@ class GeoMesaLabelParser extends FunctionExpressionImpl(
     parameter("property2", classOf[String]),
     parameter("property3", classOf[String]))) {
 
-  val reg = ".+\\w.+"
-
-  def formatProp (prop: String, format: String): String = {
-    if (prop == "") {
+  def formatProp (expr: Any, format: String): String = {
+    if (expr == null) {
       ""
     } else {
-      if (prop.matches(reg)) { prop } else {
+      // expr could be an Int/Float/Double
+      val prop = expr.toString
+      // If there is a letter in the prop value, we can't parse so short circuit
+      if (prop.matches(".+\\w.+") || prop == "") {
+        prop
+      } else {
         try {
           jDouble.parseDouble(prop).formatted(format)
         } catch {
@@ -39,19 +42,10 @@ class GeoMesaLabelParser extends FunctionExpressionImpl(
   }
 
   override def evaluate(o: jObject): AnyRef = {
-    val exp1 = getExpression(1).evaluate(o)
-    val exp2 = getExpression(2).evaluate(o)
-    val exp3 = getExpression(3).evaluate(o)
-
-    val property1 = if (exp1 == null) { "" } else exp1.toString
-    val property2 = if (exp2 == null) { "" } else exp2.toString
-    val property3 = if (exp3 == null) { "" } else exp3.toString
-
-    if (property1 + property2 + property3 == "") {
-      ""
-    } else {
-      val nf: String = getExpression(0).evaluate(null).asInstanceOf[String]
-      formatProp(property1, nf) + "\n" + formatProp(property2, nf) + "\n" + formatProp(property3, nf)
-    }
+    val expr1 = getExpression(1).evaluate(o)
+    val expr2 = getExpression(2).evaluate(o)
+    val expr3 = getExpression(3).evaluate(o)
+    val nf: String = getExpression(0).evaluate(null).asInstanceOf[String]
+    formatProp(expr1, nf) + "\n" + formatProp(expr2, nf) + "\n" + formatProp(expr3, nf)
   }
 }
