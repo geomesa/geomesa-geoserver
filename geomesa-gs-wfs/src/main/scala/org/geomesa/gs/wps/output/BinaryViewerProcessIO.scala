28677d28e0d36209d7bd87371756b243cbde3a77
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2017-08-10 14:43:48 -0400

    Bumping geomesa version to 1.3.3
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-gs-wfs/src/main/scala/org/geomesa/gs/wps/output/BinaryViewerProcessIO.scala
+++ geomesa-gs-wfs/src/main/scala/org/geomesa/gs/wps/output/BinaryViewerProcessIO.scala
@@ -13,9 +13,6 @@ import java.io.{InputStream, OutputStream}
 import org.geomesa.gs.wfs.output.BinaryViewerOutputFormat
 import org.geoserver.wps.ppio.BinaryPPIO
 import org.geoserver.wps.ppio.ProcessParameterIO.PPIODirection
-import org.geotools.data.simple.SimpleFeatureCollection
-import org.geotools.feature.FeatureCollection
-import org.locationtech.geomesa.filter.function.BinaryOutputEncoder
 
 /**
   * WPS output formatting for binary viewer. Does not support input formatting.
@@ -28,30 +25,6 @@ class BinaryViewerProcessIO extends BinaryPPIO(classOf[java.util.Iterator[Array[
   override def encode(value: AnyRef, os: OutputStream): Unit = {
     import scala.collection.JavaConversions._
     value.asInstanceOf[java.util.Iterator[Array[Byte]]].foreach(os.write)
-  }
-
-  override def decode(input: InputStream): AnyRef = throw new NotImplementedError("Only supports encode")
-
-  override def getFileExtension: String = ".bin"
-}
-
-/**
-  * WPS output formatting for binary viewer. Does not support input formatting.
-  */
-@deprecated("Use chained BinaryViewerProcess")
-class BinaryViewerFeatureCollectionProcessIO extends BinaryPPIO(classOf[FeatureCollection[_, _]],
-  classOf[FeatureCollection[_, _]], BinaryViewerOutputFormat.MIME_TYPE) {
-
-  override def getDirection: PPIODirection = PPIODirection.ENCODING
-
-  override def encode(value: AnyRef, os: OutputStream): Unit = {
-    import scala.collection.JavaConversions._
-    val fc = value.asInstanceOf[SimpleFeatureCollection]
-    val options = BinaryOutputEncoder.CollectionEncodingOptions.remove(fc.getID)
-    if (options == null) {
-      throw new IllegalArgumentException("BIN process output requires configuration of BIN values in request")
-    }
-    BinaryOutputEncoder.encodeFeatureCollection(fc, os, options, sort = false)
   }
 
   override def decode(input: InputStream): AnyRef = throw new NotImplementedError("Only supports encode")
