9d32805c93d8815c4efa0c050b156b49bce9a576
Nick Phair <nicholas.phair@ccri.com>
2018-09-28 19:37:38 -0400

    No GWC.
    Signed-off-by: Nick Phair <nicholas.phair@ccri.com>

--- geomesa-gs-catalog-listener/src/main/scala/org/geomesa/gs/catalog/AutoRegisterFilter.scala
+++ geomesa-gs-catalog-listener/src/main/scala/org/geomesa/gs/catalog/AutoRegisterFilter.scala
@@ -52,7 +52,7 @@ class AutoRegisterFilter extends GeoServerFilter {
   def shouldCreate(workspace: String): Boolean = {
     // Check that
     // val base = System.getProperty("GEOMESA_FSDS_BASE_DIRECTORY") + workspace exists!
-    !Seq("styles", "web", "index.html", "openlayers3").contains(workspace)
+    !Seq("gwc", "styles", "web", "index.html", "openlayers3").contains(workspace)
   }
 
   override def destroy(): Unit = { }
