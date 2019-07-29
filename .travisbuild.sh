c0f1e56ed36226b4ad1b3d9e15f2f1fbf30172df
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2019-07-29 09:33:08 -0400

    Fixing travis build output
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- .travisbuild.sh
+++ .travisbuild.sh
@@ -14,7 +14,7 @@ bash -c "while true; do sleep $PING_SLEEP; echo [INFO] \$(date -u '+%F %T UTC') 
 PING_LOOP_PID=$!
 
 # build using the maven executable, not the zinc maven compiler (which uses too much memory)
-mvn clean test 
+mvn clean test 2>&1 | tee -a $BUILD_OUTPUT | grep -e 'Building GeoMesa' -e '\(maven-surefire-plugin\|maven-jar-plugin\|scala-maven-plugin.*:compile\)'
 RESULT=${PIPESTATUS[0]} # capture the status of the maven build
 
 # dump out the end of the build log, to show success or errors
