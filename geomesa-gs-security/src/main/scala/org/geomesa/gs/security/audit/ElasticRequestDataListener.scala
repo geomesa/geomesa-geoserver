2c559b4f80743e54526b9088952b101c30cf67b9
Nate Hellmuth <nate.hellmuth@ccri.com>
2020-10-21 12:08:04 -0400

    Add dynamic host setting
    Signed-off-by: Nate Hellmuth <nate.hellmuth@ccri.com>

--- geomesa-gs-security/src/main/scala/org/geomesa/gs/security/audit/ElasticRequestDataListener.scala
+++ geomesa-gs-security/src/main/scala/org/geomesa/gs/security/audit/ElasticRequestDataListener.scala
@@ -9,6 +9,7 @@
 package org.geomesa.gs.security.audit
 
 import java.lang.reflect.Type
+import java.net.{MalformedURLException, URL}
 import java.util.Date
 
 import com.google.gson._
@@ -22,33 +23,52 @@ import org.elasticsearch.action.DocWriteResponse
 import org.geoserver.monitor.{RequestData, RequestDataListener}
 import org.opengis.geometry.BoundingBox
 
-class ElasticRequestDataListener extends RequestDataListener {
-  val host = sys.env.get("ELASTICSEARCH_HOST")
-  val index = sys.env.get("GEOSERVER_ES_INDEX")
+
+
+class ElasticRequestDataListener extends RequestDataListener with LazyLogging {
+  val strUrl = sys.env.getOrElse("ELASTICSEARCH_HOST", null)
+  var host : String = "localhost"
+  var port = 9200
+  var protocol : String = "http"
+  try {
+    val url = new URL(strUrl)
+    host = url.getHost
+    port = url.getPort
+    if (port == -1){
+      port = url.getDefaultPort()
+    }
+    protocol = url.getProtocol
+  }
+  catch {
+    case e: MalformedURLException =>
+      logger.error("Bad URL given. Could not resolve " + strUrl)
+      throw new Exception("Given URL cannot be read by Java URL")
+  }
+  val index = sys.env.getOrElse("GEOSERVER_ES_INDEX", null)
   val client = new RestHighLevelClient(
     RestClient.builder(
-      new HttpHost("localhost", 9200, "http")
+      new HttpHost(host, port, protocol)
     )
   )
   private val gson: Gson = new GsonBuilder()
-    .registerTypeAdapter(classOf[Date], new DateSerializer)
-    .registerTypeAdapter(classOf[BoundingBox], new BoundingBoxSerializer)
-    .registerTypeAdapter(classOf[Throwable], new ThrowableSerializer)
+    .registerTypeAdapter(classOf[Date], DateSerializer)
+    .registerTypeAdapter(classOf[BoundingBox], BoundingBoxSerializer)
+    .registerTypeAdapter(classOf[Throwable], ThrowableSerializer)
     .serializeNulls().create()
 
-  class DateSerializer extends JsonSerializer[Date] {
+  object DateSerializer extends JsonSerializer[Date] {
     override def serialize(src: Date, typeOfSrc: Type, context: JsonSerializationContext): JsonElement = {
       new JsonPrimitive(src.getTime)
     }
   }
 
-   class BoundingBoxSerializer extends JsonSerializer[BoundingBox] {
+   object BoundingBoxSerializer extends JsonSerializer[BoundingBox] {
      override def serialize(src: BoundingBox, typeOfSrc: Type, context: JsonSerializationContext): JsonElement = {
        new JsonPrimitive(src.toString)
      }
    }
 
-  class ThrowableSerializer extends JsonSerializer[Throwable] {
+  object ThrowableSerializer extends JsonSerializer[Throwable] {
     override def serialize(src: Throwable, typeOfSrc: Type, context: JsonSerializationContext): JsonElement = {
       new JsonPrimitive(src.getMessage)
     }
@@ -71,7 +91,7 @@ class ElasticRequestDataListener extends RequestDataListener {
       (!(requestData.getStatus == RequestData.Status.FAILED && requestData.getEndTime == null))
     ) {
       val json = gson.toJson(requestData)
-      val request = new IndexRequest("geoserver")
+      val request = new IndexRequest(index)
       request.id(s"${requestData.getId}:${requestData.getStartTime}")
       request.source(json, XContentType.JSON)
       client.indexAsync(request, RequestOptions.DEFAULT, new LoggingCallback())
